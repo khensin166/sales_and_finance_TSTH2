9946223d941f5836e5dac6daa49fad7cd870ddcc
Kenan Tomfie Bukit <119487201+khensin166@users.noreply.github.com>
2025-06-16 08:19:29 +0700

    delete

--- sales_and_finance/sales/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SalesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sales'
-
-    # def ready(self):
-    #     import sales.signals  # Ganti `your_app` dengan nama aplikasimu
--- sales_and_finance/update_expiry_milk.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd D:\Kuliah\Semester 6\TA\sales_and_finance_TSTH2s\sales_and_finance
-call C:\Users\khens\Envs\notesenv\Scripts\activate.bat
-python manage.py update_expiry_milk
